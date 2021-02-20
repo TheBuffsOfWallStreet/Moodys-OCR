e73427158d4e5e3f060ffd12a68cb0d629db4481
Andrew Yee <andrewyee@me.com>
2021-02-20 13:05:18 -0700

    implement function to connect to database and insert data

--- src/parse_orc.py
+++ src/parse_orc.py
@@ -1,5 +1,6 @@
 import os
 import re
+import getpass
 from pymongo import MongoClient
 
 def parse_OCR(path, page, year, brightness=60):
@@ -25,3 +26,31 @@ def parse_OCR(path, page, year, brightness=60):
             res.append(entry)
     return res
 
+def insert_year(path, year):
+    '''
+    Input directory path to OCR data and year of book
+    Ex:
+        path: "/scratch/summit/diga9728/Moodys/Industrials/OCRrun1936/"
+        year: 1936
+    Inserts the OCR data from parsed files into database
+    '''
+    pwd = getpass.getpass("Password: ")
+    db = MongoClient(host="royceschultz.com", port=27017, username="finlab_beta", password=pwd, authSource="users").finlab_dev.asy_MoodysOCR
+    dirs = os.listdir(path)
+    sub_dirs = []
+    for d in dirs:
+        match = re.fullmatch('[0-9]{3}', d)
+        if match:
+            sub_dirs.append(match.group(0))
+    for d in sub_dirs:
+        sub_path = os.path.join(path, d)
+        files = os.listdir(sub_path)
+        for f in files:
+            if '.zay' in f:
+                try:
+                    page = int(f[-8:-4])
+                    docs = parse_OCR(os.path.join(sub_path, f), page, year)
+                    db.insert_many(docs)
+                except Exception as e:
+                    print(f)
+                    print(e)
