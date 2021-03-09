8f4c5694b10c4263ba983ef049716edc2aa34729
Andrew Yee <andrewyee@me.com>
2021-03-08 19:49:16 -0700

    implement script to parse header data and insert the result into database

--- src/header_data.py
+++ src/header_data.py
@@ -2,6 +2,29 @@ import getpass
 from pymongo import MongoClient
 
 def parse_header(path):
+    '''
+    Takes in a file path for the header data to be parsed
+    Returns a list of dictionary objects of header data
+    '''
+    res = []
     with open(path) as file:
         lines = file.readlines()
-        
+        for line in lines:
+            data = line.split(',')
+            entry = {
+                'company': data[0].strip('"'),
+                'image_id': int(data[1]),
+                'x1': float(data[2]),
+                'x2': float(data[3]),
+                'y1': float(data[4]),
+                'y2': float(data[5])
+            }
+            res.append(entry)
+    return res
+
+
+if __name__ == "__main__":
+    pwd = getpass.getpass("Password: ")
+    headers = parse_header("GoldStandard1930s.20200923.csv")
+    db = MongoClient(host="royceschultz.com", port=27017, username='finlab_beta', password=pwd, authSource="users").finlab_dev.yee_GoldHeaders
+    db.insert_many(headers)
